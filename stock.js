5d2111b5d3eb5ef0f1fdbb6e7756d3de41244892
projektreceptar <projektreceptar@gmail.com>
2025-08-26 11:33:50 +0200

    Update stock.js

--- stock.js
+++ stock.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
             await fetch(SAVE_WEBHOOK_URL, {
                 method: 'POST',
-                headers: { 'Content-Type': 'text/plain' },
+                headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data)
             });
                 window.close();
